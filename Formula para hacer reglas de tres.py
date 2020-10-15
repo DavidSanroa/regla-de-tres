dfc90b39c6ac56faff51239141a70fef2ce334c7
DavidSanroa <40637739+DavidSanroa@users.noreply.github.com>
2020-10-15 12:18:42 +0200

    Add files via upload

---
+++ Formula para hacer reglas de tres.py
@@ -0,0 +1,48 @@
+# Leer una medida en pies
+# Mostrar distancia equivalente en pulgadas, yardas y millas
+# 1 pie = 12 pulgadas
+# 1 pie = 0.333333 yardas
+# 1 pie = 0,000189394 millas
+
+# Fórmula (A/B) = (x/C) ; x = (A * C) / B ;
+# Reformula (A/B) = (C/x) ; x = (A/B) * C ;
+
+a = 2
+b = 2
+c = 2
+
+x = (a * c) / b
+
+hypothesis_one = a / b
+
+hypothesis_two = x / c
+
+print(hypothesis_one)
+
+print(hypothesis_two)
+
+if hypothesis_one == hypothesis_two:
+    print('Your hypothesis is right')
+else:
+    print('Your hypothesis is wrong')
+
+d = 2
+e = 2
+f = 2
+
+y = (d / e) * f
+
+hypothesis_three = d / e
+ 
+hypothesis_four = f / y
+
+print(hypothesis_three)
+
+print(hypothesis_four)
+
+if hypothesis_three == hypothesis_four:
+    print('Your hypothesis is right')
+else:
+    print('Your hypothesis is wrong')
+
+# seguir con un if else
